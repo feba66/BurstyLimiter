02fa4fed3c03d396a9678ba6049a623cef575d82
feba66 Felix Baumkötter <feba66@web.de>
2025-01-17 23:14:11 +0100

    tests and readme

--- tests/test_burst.py
+++ tests/test_burst.py
@@ -1,16 +1,16 @@
 import time
-from burstylimiter import Limiter
+from burstylimiter import Limiter, BurstyLimiter
 
 
-@Limiter(1, 1)
+@BurstyLimiter(Limiter(1, 1), Limiter(1, 3))
 def ratelimited_function():
     return
 
 
-def test_limiter():
+def test_burst():
     t0 = time.time()
     for _ in range(3):
         ratelimited_function()
     t1 = time.time()
     diff = t1 - t0
-    assert diff > 2 and diff < 2.5
+    assert diff > 1 and diff < 1.4
--- tests/test_limiter2.py
+++ tests/test_limiter2.py
@@ -7,10 +7,10 @@ def ratelimited_function():
     return
 
 
-def test_limiter():
+def test_min_time():
     t0 = time.time()
-    for _ in range(3):
+    for _ in range(1):
         ratelimited_function()
     t1 = time.time()
     diff = t1 - t0
-    assert diff > 2 and diff < 2.5
+    assert diff < 0.4 and diff > 0
